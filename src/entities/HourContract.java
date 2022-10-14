6f5fa7b3474faeb473e08de394726fd418f8ed4d
José Marcos <Jmteles18@gmail.comgit config --global user.email Jmteles18@gmail.com>
2022-10-14 00:19:19 -0300

    Criado projeto composicao

---
+++ src/entities/HourContract.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+	private Date date;
+	private Double valuePerHour;
+	private Integer hours;
+	
+	
+	public HourContract () {
+		
+	}
+
+
+	public HourContract(Date date, Double valuePerHour, Integer hours) {
+		
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+	
+	public double totalValue() {
+		return valuePerHour * hours;
+		}
+
+
+	public Date getDate() {
+		
+		return null;
+	}
+
+
+
+
+
+
+
+
+
+}
+
